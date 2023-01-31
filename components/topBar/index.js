a8b0a81c919fceff9663c86e7c9a4ca97be30d79
ojlennon <45679125+ojlennon@users.noreply.github.com>
2023-01-31 13:28:20 -0700

    not working

--- components/topBar/index.js
+++ components/topBar/index.js
@@ -1,25 +1,38 @@
 import { Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { Button, Box, IconButton } from "@react-native-material/core";
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import { TouchableOpacity } from 'react-native';
+import { MenuOutlined } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const TopBar = () => {
     const [s, setS] = useState(false)
     return (
         <>
-        <Box width="100%" height="20%" backgroundColor="#c0c0c0" paddingTop="15%" alignItems="center"> 
-        <IconButton onPress={() => setS(!s)}>
-                <MenuOutlinedIcon sx={{color:"black"}}/>
-            </IconButton>
-            </Box>
+            <View>
+
+            <Box width="100%" height="20%" backgroundColor="#c0c0c0" paddingTop="15%" alignItems="center"> 
+                <IconButton onPress={() => setS(!s)}>
+                        {/* <MenuOutlinedIcon color="primary" /> */}
+                        <MenuIcon/>
+                </IconButton>
+                </Box>
+                <IconButton onPress={() => setS(!s)}>
+                        {/* <MenuOutlinedIcon color="primary" /> */}
+                        <MenuIcon color='primary'/>
+                </IconButton>
           
-            {s &&
-                <Box mt="60%" backgroundColor="blue" fontSize="30px">
-            <Text>
-                HELOOOOOO
-            </Text>
-        </Box>}
+                
+                    <Box width="100%" mt="60%" backgroundColor="blue" fontSize="30px">
+                        {/* <Text>
+                            HELOOOOOO
+                        </Text> */}
+                    <Button title="Fu"/>
+                        {/* <MenuIcon/> */}
+                        {/* <MenuOutlinedIcon color="primary"/> */}
+                    </Box>
+            </View>
         </>
     )
 }
